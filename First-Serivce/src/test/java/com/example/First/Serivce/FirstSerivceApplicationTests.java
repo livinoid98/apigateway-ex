128e75f04eef6aa3fdf429ae1f2b30e12349f234
chanyoung.choi <chanyoung.choi@innowireless.com>
2024-03-11 14:35:43 +0900

    [#2][Filter] global & custom filter 설정

---
+++ First-Serivce/src/test/java/com/example/First/Serivce/FirstSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.First.Serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Second-Serivce/src/test/java/com/example/Second/Serivce/SecondSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Second.Serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
