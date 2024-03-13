3f0bfe828063446848934424e35bd2658bd6f8e9
chanyoung.choi <chanyoung.choi@innowireless.com>
2024-03-13 16:23:30 +0900

    [#3][db] h2 database 설정

--- First-Serivce/src/main/java/com/example/First/Serivce/FirstServiceController.java
+++ First-Serivce/src/main/java/com/example/First/Serivce/FirstServiceController.java
@@ -1,6 +1,10 @@
 package com.example.First.Serivce;
 
+import com.netflix.discovery.converters.Auto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +34,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
